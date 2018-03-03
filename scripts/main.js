5a78c7e093edc757b6fb396d6d21e25c0f30819a
iamdevlinph <iamdevlinph@gmail.com>
2018-03-03 18:50:03 +0800

    Some random stuff

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+(animateSocialLinks = () => {
+  document.addEventListener("DOMContentLoaded", function (event) {
+    document.getElementById('social-links-container').className = 'animated flipInX';
+    console.log('oh, hi mark')
+  });
+})();
+
+const tagLines = [
+  "JavaScript Developer",
+  "Good day for fishin' ain't it?",
+  "Why can't we screenshot the brightness of our phone? Hmm",
+  "Civil War: Spaces vs Tabs",
+  ";",
+  "Still looking for the semi-colon",
+  "I did not hit her"
+]
+
+const tagLineP = document.getElementById('tag-line');
+
+document.onreadystatechange = function (e) {
+  if (document.readyState === 'complete') {
+    tagLineP.style.display = 'block';
+    tagLineP.innerHTML = getRandomTag(tagLines);
+  }
+};
+
+getRandomTag = (tagLines) => {
+  const len = tagLines.length;
+  return tagLines[Math.floor(Math.random() * len) + 0];
+}
