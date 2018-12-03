0f8a9604f47c33a61f4cc1e6ab7c5fd382fcbf1d
iamdevlinph <iamdevlinph@gmail.com>
2018-12-03 16:28:10 +0800

    Eslint react in jsx scope

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { MainLayout } from '../src/layouts';
 
 export default () => (
--- src/layouts/MainLayout/MainLayout.js
+++ src/layouts/MainLayout/MainLayout.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Head, Navbar } from '../../components';
 import './MainLayout.scss';
