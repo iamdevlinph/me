a9bf0df6d6e53656e15bab4107a1808809c52826
iamdevlinph <iamdevlinph@gmail.com>
2018-12-16 20:54:40 +0800

    Changes to next.config.json zeit/next.js#5750

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
-// next.config.js
-const withSass = require('@zeit/next-sass');
+/* eslint-disable no-nested-ternary, no-unused-vars, global-require, import/no-unresolved */
+const { PHASE_PRODUCTION_SERVER } = process.env.NODE_ENV === 'development'
+  ? {} // We're never in "production server" phase when in development mode
+  : !process.env.NOW_REGION
+    ? require('next/constants') // Get values from `next` package when building locally
+    : require('next-server/constants'); // Get values from `next-server` package when building on now v2
 
-module.exports = withSass();
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_PRODUCTION_SERVER) {
+    // Config used to run in production.
+    return {};
+  }
+
+  const withSass = require('@zeit/next-sass');
+
+  return withSass();
+};
